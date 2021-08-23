3b8182ecc3a9ee305fef4ac57362d0ba547f6ee1
js-job <grawires@gmail.com>
2021-08-23 14:51:04 +0200

    ...

---
+++ cutter1d/src/main/java/pl/printo3d/onedcutter/cutter1d/cutter/models/ResultBar.java
@@ -0,0 +1,25 @@
+﻿package pl.printo3d.onedcutter.cutter1d.cutter.models;
+
+public class ResultBar {
+
+  public ResultBarPieceModel[] resultBarPieceModel;
+
+
+  public ResultBar(ResultBarPieceModel[] resultBarPieceModel) {
+    this.resultBarPieceModel = resultBarPieceModel;
+  }
+
+
+  public ResultBarPieceModel[] getResultBarPieceModel() {
+    return resultBarPieceModel;
+  }
+
+
+  public void setResultBarPieceModel(ResultBarPieceModel[] resultBarPieceModel) {
+    this.resultBarPieceModel = resultBarPieceModel;
+  }
+
+  
+  
+  
+}
