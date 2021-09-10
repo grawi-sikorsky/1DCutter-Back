b88bc06bd1338593a7ede45a6a9ba039e359aab3
js <grawires@gmail.com>
2021-09-10 21:42:23 +0200

    mock

--- src/test/java/pl/printo3d/onedcutter/cutter1d/cutter/services/ProstyTest.java
+++ src/test/java/pl/printo3d/onedcutter/cutter1d/cutter/services/ProstyTest.java
@@ -1,39 +1,23 @@
 package pl.printo3d.onedcutter.cutter1d.cutter.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.spy;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-
-import com.mysql.cj.xdevapi.Result;
-
-import org.aspectj.lang.annotation.Before;
-import org.hamcrest.Matchers;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
-
-import pl.printo3d.onedcutter.cutter1d.cutter.models.StockModel;
-import pl.printo3d.onedcutter.cutter1d.cutter.models.WorkPiece;
 import pl.printo3d.onedcutter.cutter1d.cutter.models.CutModel;
-import pl.printo3d.onedcutter.cutter1d.cutter.models.CutOptions;
-import pl.printo3d.onedcutter.cutter1d.cutter.models.OrderModel;
-import pl.printo3d.onedcutter.cutter1d.cutter.services.OrderService;
+import pl.printo3d.onedcutter.cutter1d.cutter.models.WorkPiece;
 
 class ProstyTest {
 
   @Test
-  public void calculateNeededStock_count()
+  public void calculateNeededStock_mapReturn()
   {
     //given
     ResultService serviceTest = spy(ResultService.class);
@@ -42,26 +26,11 @@ class ProstyTest {
     Map<Double,Integer> a = serviceTest.calculateNeededStock(prepareMockWorkPiece());
 
     //then
-    assertEquals(a.size(), 2); 
+    assertEquals(a.size(), 2); // map (key: 1000, val: 2) i (key: 2000, val 2)
   }
-
-  @Test
-  public void calculateNeededStock_test1_succes()
-  {
-    //given
-    ResultService serviceTest = spy(ResultService.class);
-    
-    //when
-    Map<Double,Integer> a = serviceTest.calculateNeededStock(prepareMockWorkPiece());
-
-    //then
-    assertEquals(a.size(), 2); 
-  }
-
   private List<WorkPiece> prepareMockWorkPiece()
   {
     List<WorkPiece> workPieces = new ArrayList<WorkPiece>();
-
     // 2 x 1000
     workPieces.add(new WorkPiece(1000.0));
     workPieces.add(new WorkPiece(1000.0));
@@ -69,8 +38,31 @@ class ProstyTest {
     workPieces.add(new WorkPiece(2000.0));
     workPieces.add(new WorkPiece(2000.0));
 
-    workPieces.add(new WorkPiece(1100.0));
+    return workPieces;
+  }
+
+  @Test
+  public void calculateWaste()
+  {
+    //given
+    ResultService serviceTest = spy(ResultService.class);
+    
+    //when
+    Double result = serviceTest.calculateWaste( prepareMockWorkPiece2() );
+
+    //then
+    assertEquals(result, 10); // 100 - 90 = 10%
+  }
+  private List<WorkPiece> prepareMockWorkPiece2()
+  {
+    List<WorkPiece> workPieces = new ArrayList<WorkPiece>();
+    workPieces.add(new WorkPiece(100.0));
+    workPieces.add(new WorkPiece(100.0));
+    workPieces.get(0).cut(90.0);
+    workPieces.get(1).cut(90.0);
 
     return workPieces;
   }
+
+
 }
