1a563464d01ac028d6cafa3dc5d246fd5a926a8e
js-job <grawires@gmail.com>
2021-09-13 16:57:20 +0200

    Update ProstyTest.java

--- src/test/java/pl/printo3d/onedcutter/cutter1d/cutter/services/ProstyTest.java
+++ src/test/java/pl/printo3d/onedcutter/cutter1d/cutter/services/ProstyTest.java
@@ -1,34 +1,15 @@
 package pl.printo3d.onedcutter.cutter1d.cutter.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.spy;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-
-import com.mysql.cj.xdevapi.Result;
-
-import org.aspectj.lang.annotation.Before;
-import org.hamcrest.Matchers;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
-
-import pl.printo3d.onedcutter.cutter1d.cutter.models.StockModel;
 import pl.printo3d.onedcutter.cutter1d.cutter.models.WorkPiece;
-import pl.printo3d.onedcutter.cutter1d.cutter.models.CutModel;
-import pl.printo3d.onedcutter.cutter1d.cutter.models.CutOptions;
-import pl.printo3d.onedcutter.cutter1d.cutter.models.OrderModel;
-import pl.printo3d.onedcutter.cutter1d.cutter.services.OrderService;
 
 class ProstyTest {
 
@@ -69,8 +50,7 @@ class ProstyTest {
     workPieces.add(new WorkPiece(2000.0));
     workPieces.add(new WorkPiece(2000.0));
 
-    workPieces.add(new WorkPiece(1100.0));
-
+//
     return workPieces;
   }
 }
