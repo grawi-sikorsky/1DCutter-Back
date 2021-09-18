c4c12e60960b7069ed9b571c456579a8e3eb7322
UKE12373938 <jakub.piecuch1@tesco.com>
2021-09-18 15:58:12 +0200

    prywatne pola na modelach/encjach/obiektach

--- src/main/java/pl/printo3d/onedcutter/cutter1d/cutter/models/CutOptions.java
+++ src/main/java/pl/printo3d/onedcutter/cutter1d/cutter/models/CutOptions.java
@@ -8,47 +8,55 @@ import javax.persistence.Id;
 @Entity
 public class CutOptions {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  public boolean optionStackResult;
-  public Double optionSzrank;
-  public boolean optionPrice;
-
-
-  public CutOptions() {}
-
-  public CutOptions(boolean optionStackResult, Double optionSzrank, boolean optionPrice) {
-    this.optionStackResult = optionStackResult;
-    this.optionSzrank = optionSzrank;
-    this.optionPrice = optionPrice;
-  }
+    private boolean optionStackResult;
+    private Double optionSzrank;
+    private boolean optionPrice;
 
 
-  public boolean isOptionStackResult() {
-    return optionStackResult;
-  }
-  public void setOptionStackResult(boolean optionStackResult) {
-    this.optionStackResult = optionStackResult;
-  }
-  public Double getOptionSzrank() {
-    return optionSzrank;
-  }
-  public void setOptionSzrank(Double optionSzrank) {
-    this.optionSzrank = optionSzrank;
-  }
-  public Long getId() {
-    return id;
-  }
-  public void setId(Long id) {
-    this.id = id;
-  }
-  public boolean isOptionPrice() {
-    return optionPrice;
-  }
-  public void setOptionPrice(boolean optionPrice) {
-    this.optionPrice = optionPrice;
-  }
-  
+    public CutOptions() {
+    }
+
+    public CutOptions(boolean optionStackResult, Double optionSzrank, boolean optionPrice) {
+        this.optionStackResult = optionStackResult;
+        this.optionSzrank = optionSzrank;
+        this.optionPrice = optionPrice;
+    }
+
+
+    public boolean isOptionStackResult() {
+        return optionStackResult;
+    }
+
+    public void setOptionStackResult(boolean optionStackResult) {
+        this.optionStackResult = optionStackResult;
+    }
+
+    public Double getOptionSzrank() {
+        return optionSzrank;
+    }
+
+    public void setOptionSzrank(Double optionSzrank) {
+        this.optionSzrank = optionSzrank;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isOptionPrice() {
+        return optionPrice;
+    }
+
+    public void setOptionPrice(boolean optionPrice) {
+        this.optionPrice = optionPrice;
+    }
+
 }
--- src/main/java/pl/printo3d/onedcutter/cutter1d/cutter/services/ResultService.java
+++ src/main/java/pl/printo3d/onedcutter/cutter1d/cutter/services/ResultService.java
@@ -1,136 +1,123 @@
-﻿package pl.printo3d.onedcutter.cutter1d.cutter.services;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+package pl.printo3d.onedcutter.cutter1d.cutter.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import pl.printo3d.onedcutter.cutter1d.cutter.models.ResultBar;
 import pl.printo3d.onedcutter.cutter1d.cutter.models.ResultBarPieceModel;
 import pl.printo3d.onedcutter.cutter1d.cutter.models.ResultModel;
 import pl.printo3d.onedcutter.cutter1d.cutter.models.WorkPiece;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Service
 public class ResultService {
 
-  @Autowired
-  private OneDCutService cutService;
+    @Autowired
+    private OneDCutService cutService;
 
-  private ResultModel fullResults = new ResultModel();
+    private ResultModel fullResults = new ResultModel();
 
-  private ResultBar resultBar = new ResultBar();
+    private ResultBar resultBar = new ResultBar();
 
-  public ResultService(){}
-  
-
-  // TODO !!! CALA USLUGA DO PRZEROBIENIA ! 4/5 metod iteruje po "List<WorkPiece>"!
-  // TODO DO ZEBRANIA W JEDNA METODE!
-
-  public List<ResultBar> getResultsBars(List<WorkPiece> workPieces)
-  {
-    List<ResultBar> resultBars = new ArrayList<ResultBar>();
-    resultBars.clear();
-
-    // POMYSL:
-    // 1 STREAM JAKOSC ZROBIC I USUNAC WSZYSTKIE DUPLIKATY
-    // 2 POTEM ITEROWAC NOWO POWSTALA TABLICA I SPRAWDZIC CZY W PIERWODNEJ WYSTEPUJA TAKIE ELEMENTY I W JAKIEJ ILOSCI.
-    // 3 ZROZUMIEC ZE TO WSZYSTKO NIE MA SENSU...
-
-    for (WorkPiece wp : workPieces)
-    {
-      for(int i=0; i < wp.cuts.size(); ++i)
-      {
-        resultBar.addPiece(new ResultBarPieceModel((String.valueOf(  (wp.cuts.get(i) / wp.getStockLenght()) * 100)), String.valueOf(wp.cuts.get(i))));
-      }
-      resultBars.add(new ResultBar( new ArrayList<ResultBarPieceModel>(resultBar.resultBarPieces), wp.getStockLenght()  ));
-      resultBar.clear();
+    public ResultService() {
     }
-    return resultBars;
-  }
 
-  public List<ResultBar> getRemainBars(List<Double> remainPcs)
-  {
-    List<ResultBar> remainBars = new ArrayList<ResultBar>();
-    remainBars.clear();
 
-    for(Double rp : remainPcs)
-    {
-      resultBar.addPiece(new ResultBarPieceModel((String.valueOf(  (rp / 1000) * 100)), String.valueOf(rp)));
+    // TODO !!! CALA USLUGA DO PRZEROBIENIA ! 4/5 metod iteruje po "List<WorkPiece>"!
+    // TODO DO ZEBRANIA W JEDNA METODE!
 
-      remainBars.add(new ResultBar( new ArrayList<ResultBarPieceModel>(resultBar.resultBarPieces)  ));
-      resultBar.clear();
-    }
-    setResultRemainingPieces(remainBars);
+    public List<ResultBar> getResultsBars(List<WorkPiece> workPieces) {
+        List<ResultBar> resultBars = new ArrayList<ResultBar>();
+        resultBars.clear();
 
-    return remainBars;
-  }
+        // POMYSL:
+        // 1 STREAM JAKOSC ZROBIC I USUNAC WSZYSTKIE DUPLIKATY
+        // 2 POTEM ITEROWAC NOWO POWSTALA TABLICA I SPRAWDZIC CZY W PIERWODNEJ WYSTEPUJA TAKIE ELEMENTY I W JAKIEJ ILOSCI.
+        // 3 ZROZUMIEC ZE TO WSZYSTKO NIE MA SENSU...
 
-  public Double calculateWaste(List<WorkPiece> workPieces)
-  {
-    Double resultWaste=0.0;
-    Double resultUsed=0.0;
-    Double resultWasteProcent=0.0;
-
-    for (WorkPiece workpc : workPieces) 
-    {
-      resultUsed += workpc.getStockLenght();
-      resultWaste += workpc.freeSpace(0.0);
-    }
-    resultWasteProcent = (resultWaste / resultUsed) * 100.0;
-    fullResults.setResultUsed(resultUsed);
-    fullResults.setResultWasteProcent(resultWasteProcent);
-    fullResults.setResultUsedProcent(100 - resultWasteProcent);
-
-    return resultWasteProcent;
-  }
-
-  public Map<Double,Integer> calculateNeededStock(List<WorkPiece> workPieces)
-  {
-    Map<Double,Integer> resultNeededStock = new HashMap<Double,Integer>();
-    resultNeededStock.clear();
-
-    for (WorkPiece workpc : workPieces)
-    {
-      if (resultNeededStock.get(workpc.getStockLenght()) == null) { resultNeededStock.put(workpc.getStockLenght(), 1); } // initial zeby nie lecialo NPE
-      else resultNeededStock.put( workpc.getStockLenght(), resultNeededStock.get(workpc.getStockLenght())+1 );
-    }
-    return resultNeededStock;
-  }
-
-  public Double calculatePrice(List<WorkPiece> workPieces)
-  {
-    Double costs=0D;
-
-    for (WorkPiece workpc : workPieces)
-    {
-      for (int index=0; index < this.cutService.stockList.size(); index++)
-      {
-        if( this.cutService.stockList.get(index).getIdFront().equals(workpc.getFrontID()))
-        {
-          costs += Double.valueOf(this.cutService.stockList.get(index).getStockPrice());
+        for (WorkPiece wp : workPieces) {
+            for (int i = 0; i < wp.getCuts().size(); ++ i) {
+                resultBar.addPiece(new ResultBarPieceModel((String.valueOf((wp.getCuts().get(i) / wp.getStockLenght()) * 100)), String.valueOf(wp.getCuts().get(i))));
+            }
+            resultBars.add(new ResultBar(new ArrayList<ResultBarPieceModel>(resultBar.getResultBarPieces()), wp.getStockLenght()));
+            resultBar.clear();
         }
-      }
+        return resultBars;
     }
-    return costs;
-  }
-  
+
+    public List<ResultBar> getRemainBars(List<Double> remainPcs) {
+        List<ResultBar> remainBars = new ArrayList<ResultBar>();
+        remainBars.clear();
+
+        for (Double rp : remainPcs) {
+            resultBar.addPiece(new ResultBarPieceModel((String.valueOf((rp / 1000) * 100)), String.valueOf(rp)));
+
+            remainBars.add(new ResultBar(new ArrayList<ResultBarPieceModel>(resultBar.getResultBarPieces())));
+            resultBar.clear();
+        }
+        setResultRemainingPieces(remainBars);
+
+        return remainBars;
+    }
+
+    public Double calculateWaste(List<WorkPiece> workPieces) {
+        Double resultWaste = 0.0;
+        Double resultUsed = 0.0;
+        Double resultWasteProcent = 0.0;
+
+        for (WorkPiece workpc : workPieces) {
+            resultUsed += workpc.getStockLenght();
+            resultWaste += workpc.freeSpace(0.0);
+        }
+        resultWasteProcent = (resultWaste / resultUsed) * 100.0;
+        fullResults.setResultUsed(resultUsed);
+        fullResults.setResultWasteProcent(resultWasteProcent);
+        fullResults.setResultUsedProcent(100 - resultWasteProcent);
+
+        return resultWasteProcent;
+    }
+
+    public Map<Double, Integer> calculateNeededStock(List<WorkPiece> workPieces) {
+        Map<Double, Integer> resultNeededStock = new HashMap<Double, Integer>();
+        resultNeededStock.clear();
+
+        for (WorkPiece workpc : workPieces) {
+            if (resultNeededStock.get(workpc.getStockLenght()) == null) {
+                resultNeededStock.put(workpc.getStockLenght(), 1);
+            } // initial zeby nie lecialo NPE
+            else resultNeededStock.put(workpc.getStockLenght(), resultNeededStock.get(workpc.getStockLenght()) + 1);
+        }
+        return resultNeededStock;
+    }
+
+    public Double calculatePrice(List<WorkPiece> workPieces) {
+        Double costs = 0D;
+
+        for (WorkPiece workpc : workPieces) {
+            for (int index = 0; index < this.cutService.stockList.size(); index++) {
+                if (this.cutService.stockList.get(index).getIdFront().equals(workpc.getFrontID())) {
+                    costs += Double.valueOf(this.cutService.stockList.get(index).getStockPrice());
+                }
+            }
+        }
+        return costs;
+    }
 
 
-  public ResultModel makeFullResults()
-  {
-    fullResults.setResultCutCount(this.cutService.workPiecesList.size());
-    fullResults.setResultNeededStock(this.calculateNeededStock(this.cutService.workPiecesList));
-    fullResults.setResultBars(this.getResultsBars(this.cutService.workPiecesList));
-    fullResults.setResultWaste(this.calculateWaste(this.cutService.workPiecesList));
-    fullResults.setResultCostOveral(this.calculatePrice(this.cutService.workPiecesList));
+    public ResultModel makeFullResults() {
+        fullResults.setResultCutCount(this.cutService.workPiecesList.size());
+        fullResults.setResultNeededStock(this.calculateNeededStock(this.cutService.workPiecesList));
+        fullResults.setResultBars(this.getResultsBars(this.cutService.workPiecesList));
+        fullResults.setResultWaste(this.calculateWaste(this.cutService.workPiecesList));
+        fullResults.setResultCostOveral(this.calculatePrice(this.cutService.workPiecesList));
 
-    return fullResults;
-  }
+        return fullResults;
+    }
 
-  public void setResultRemainingPieces(List<ResultBar> remain)
-  {
-    fullResults.setResultRemainingPieces(remain);
-  }
+    public void setResultRemainingPieces(List<ResultBar> remain) {
+        fullResults.setResultRemainingPieces(remain);
+    }
 }
