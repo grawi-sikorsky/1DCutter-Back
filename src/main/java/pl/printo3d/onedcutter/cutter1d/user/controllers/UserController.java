4c2d459297d1fffc292c065a068c4c06bb7b3f4f
js-job <grawires@gmail.com>
2021-10-28 09:35:00 +0200

    Update UserController.java

--- src/main/java/pl/printo3d/onedcutter/cutter1d/user/controllers/UserController.java
+++ src/main/java/pl/printo3d/onedcutter/cutter1d/user/controllers/UserController.java
@@ -6,7 +6,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
