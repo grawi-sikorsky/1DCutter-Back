5481a21aa7251ac2eb320e1af0a47c92d178f1d2
HomePC <grawires@gmail.com>
2022-12-25 17:48:15 +0100

    Update SecurityConfig.java

--- src/main/java/pl/printo3d/onedcutter/cutter1d/SecurityConfig.java
+++ src/main/java/pl/printo3d/onedcutter/cutter1d/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
