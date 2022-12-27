b9b6e5be18ff4937ab0b8f81d3f7a695b6795d5f
HomePC <grawires@gmail.com>
2022-12-27 12:07:08 +0100

    cleanup

--- src/main/java/pl/printo3d/onedcutter/cutter1d/configuration/SwaggerConfiguration.java
+++ src/main/java/pl/printo3d/onedcutter/cutter1d/configuration/SwaggerConfiguration.java
@@ -1,18 +1,10 @@
 package pl.printo3d.onedcutter.cutter1d.configuration;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.context.SecurityContext;
 
-import io.swagger.annotations.AuthorizationScope;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -38,8 +30,4 @@ public class SwaggerConfiguration {
         .paths(PathSelectors.any())
         .build();
     }
-
-    private ApiKey apiKey() {
-        return new ApiKey("jwtToken", "Authorization", "header");
-    }    
 }
